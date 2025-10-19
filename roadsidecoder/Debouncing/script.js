00715605043b7bd16ad8471505783b1f89d527c3
alexbritto2000 <mailtoalexbritto@gmail.com>
2025-10-19 20:20:13 +0530

    debouncing

--- roadsidecoder/Debouncing/script.js
+++ roadsidecoder/Debouncing/script.js
@@ -17,8 +17,8 @@ btn.addEventListener("click", () => {
     btnTriggerFun()
 }) */
 
-//1. Throttling
-const btn = document.querySelector('.increment_btn');
+//2. Throttling
+/* const btn = document.querySelector('.increment_btn');
 const btnPress = document.querySelector('.increment_count');
 const btnTrigger = document.querySelector('.debouncing_count');
 
@@ -34,4 +34,35 @@ btn.addEventListener("click", () => {
     //console.log('hi')
     btnPress.innerHTML = ++pressedCount;
     btnTriggerFun()
+}) */
+
+//Without lodash debouncing
+const btn = document.querySelector('.increment_btn');
+const btnIncrement = document.querySelector('.increment_count');
+const btnDebouncing = document.querySelector('.debouncing_count');
+
+var incrementCount = 0;
+var debounceCount = 0;
+
+myDebounce = (cb, d) => {
+    let timer;
+
+    return function (...args) { // Capture arguments
+        if (timer) clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            cb(...args); // Pass the arguments to the callback
+        }, d);
+    }
+}
+
+
+const btnTriggerFun = myDebounce((arg)=>{
+    console.log(arg); // 'jo' will be printed here
+    btnDebouncing.innerHTML = ++debounceCount;
+},800)
+
+btn.addEventListener('click', () => {
+    btnIncrement.innerHTML = ++incrementCount;
+    btnTriggerFun('jo');
 })
